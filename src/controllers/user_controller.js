4c8a596b9f5ebe6393268e2d608b67352ae0cb6f
Keegan <juan789huarcaya@gmail.com>
2024-01-14 11:32:03 -0500

    usuario service y controller

---
+++ src/controllers/user_controller.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+  deleteUser,
+  findAll,
+  findById,
+  insertUser,
+  updateUser,
+} from "../services/user_service.js";
+const router = Router();
+
+router.get("/users", findAll);
+router.get("/users/:id_user", findById);
+router.post("/users", insertUser);
+router.patch("/users/:id_user", updateUser);
+router.delete("/users/:id_user", deleteUser);
+
+export default router;
---
+++ src/services/user_service.js
@@ -0,0 +1,95 @@
+import { pool } from "../../database.js";
+import bycript from "bcrypt";
+
+export const findAll = async (req, res) => {
+  try {
+    const [result] = await pool.query("SELECT * FROM usuarios");
+    if (result.length <= 0) {
+      return res.status(204);
+    }
+    return res.json(result);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+export const findById = async (req, res) => {
+  try {
+    const { id_user } = req.params;
+    const [result] = await pool.query(
+      "SELECT * FROM usuarios WHERE id_usuario = ?",
+      [id_user]
+    );
+    if (result.length === 0) {
+      return res.status(400).json({ message: `El usuario no existe` });
+    }
+
+    return res.json(result);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+export const insertUser = async (req, res) => {
+  try {
+    const { username, password, name, avatar } = req.body;
+    const hashedPassword = await bycript.hash(password, 10);
+    const [result] = await pool.query(
+      "INSERT INTO usuarios (name,username,password,avatar,create_date) VALUES (?,?,?,?,?)",
+      [name, username, hashedPassword, avatar, dateGenerate()]
+    );
+    if (result.affectedRows > 0) {
+      return res.status(200).json({ message: "Usuario creado" });
+    } else {
+      return res.status(400).json({ message: "Error al crear el usuario" });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { id_user } = req.params;
+    const { username, password, name, avatar } = req.body;
+    const [result] = await pool.query(
+      "UPDATE usuarios SET name = IFNULL(?,name), username = IFNULL(?,username) , avatar = IFNULL(?,avatar), password = IFNULL(?,password) WHERE id_usuario = ?",
+      [name, username, avatar, password, id_user]
+    );
+    if (result.affectedRows > 0) {
+      return res.json({ message: "Datos actualizados" });
+    } else {
+      return res.status(400).json({ message: "El usuario no existe" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id_user } = req.params;
+    const [result] = await pool.query(
+      "DELETE FROM usuarios WHERE id_usuario = ?",
+      [id_user]
+    );
+    if (result.affectedRows > 0) {
+      return res.json({ message: "usuario eliminado" });
+    } else {
+      return res.status(400).json({ message: `El usuario no existe` });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+function dateGenerate() {
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split("T")[0];
+  return formattedDate;
+}
